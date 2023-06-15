375cb809a888646cd3322ebd7b533ae1945155b0
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-06-14 23:46:47 -0300

    Criando código javascript para o navbar acompanhar a tela

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const target = document.querySelectorAll('[data-animation]');
+const animationClass = 'animation';
+
+function animationScroll() {
+    const windowTop = window.scrollY + ((window.innerHeight * 3) / 4);
+    target.forEach((e) => {
+        if (windowTop >= e.offsetTop) {
+            e.classList.add(animationClass);
+        } else {
+            e.classList.remove('animationClass');
+        }
+    })
+}
+
+if(target.length) {
+    window.addEventListener('scroll', () => {
+        animationScroll();
+    })
+}
