10b6ebdb45c8d04a8d3da9f237dbd42bb2624abf
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-06-19 17:03:15 -0300

    Corrigindo códgio javascript

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
 const navbar = document.getElementById('navbar');
 const btnTop = document.getElementById('btn-top');
 
-<<<<<<< HEAD
-const target = document.querySelectorAll('[data-animation]');
-const animationClass = 'animation';
-
-function animationScroll() {
-    const windowTop = window.scrollY + ((window.innerHeight * 3) / 4);
-    target.forEach((e) => {
-        if (windowTop >= e.offsetTop) {
-            e.classList.add(animationClass);
-        } else {
-            e.classList.remove(animationClass);
-        }
-    })
-=======
 function navbarElement (posicao) {
     if (posicao > navbar.offsetTop && posicao > btnTop.offsetTop) {
         navbar.classList.add('animation');
@@ -23,7 +9,6 @@ function navbarElement (posicao) {
         navbar.classList.remove('animation');
         btnTop.classList.remove('btn')
     }
->>>>>>> main
 }
 
 document.addEventListener('scroll', () => {
